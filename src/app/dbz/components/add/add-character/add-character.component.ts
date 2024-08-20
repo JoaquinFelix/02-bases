e9521619bae4b53aa844756ae6c3e65e9c294c75
JoaquinFelix <joaquin.felix@gmcommercial.com.mx>
2024-08-20 16:40:52 -0700

    Version con los componentes jalando de add-character y list y agregados en el modulo

---
+++ src/app/dbz/components/add/add-character/add-character.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bdz-add-character',
+  templateUrl: './add-character.component.html',
+  styleUrls: ['./add-character.component.css']
+})
+export class AddCharacterComponent {
+
+}
